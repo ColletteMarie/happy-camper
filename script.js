dedc98e9b8c9e6a3e188ec859cef8e244fc0ab2a
ColletteMarie <collettemariep@gmail.com>
2016-12-04 19:43:56 +0000

    Collette's updates 4 Dec

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+$( document ).ready(function() {
+  $( "#submit" ).click(function( event ) {
+  event.preventDefault();
+    $( ".contact-form" ).fadeOut( 300, function() {
+      $( ".thanks" ).fadeIn( 300, function() {
+      });
+    });
 
-
+  });
+});
